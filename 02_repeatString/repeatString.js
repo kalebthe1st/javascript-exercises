cf47fc84b890936b522962c007f53c4c51378360
kalebthe1st <88403660+kalebthe1st@users.noreply.github.com>
2023-11-05 07:09:03 -0800

    fixed the code for repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    if (typeof string !== "string") {
+        throw new TypeError("the first arguement must be a string")
+    }
+    if (typeof num !== "number") {
+        throw new TypeError("the second arguement must be a number")
+    }
+    if (num < 0) {
+        return "ERROR"
+    }
+    repeatedString = ""
+    for (let i = 0; i < num; i++) {
+        repeatedString += string;
 
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
